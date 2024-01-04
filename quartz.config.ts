e70773fd886989feeb1349b1f6b42c7f8d7e18b7
Daniel Barcelona <75101802+DanielBCNA@users.noreply.github.com>
2024-01-04 16:31:24 +0000

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,14 +18,14 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#f6f5f1", // original: #faf8f8 
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(82, 179, 206, 0.15)", // original: rgba(143, 159, 169, 0.15)
         },
         darkMode: {
           light: "#161618",
@@ -35,7 +35,7 @@ const config: QuartzConfig = {
           dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(82, 179, 206, 0.15)", // original: rgba(143, 159, 169, 0.15)
         },
       },
     },
